a2a038dedadd4dbf4d61928b35e4ba6dfb37653b
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-10 10:38:09 -0500

    Change name of route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/artists', to: 'artists#index'
 
-  get 'artists/:id', to: 'artists#show', as: 'show_artist'
+  get 'artists/:id', to: 'artists#show', as: 'show_artists'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
